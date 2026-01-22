0c30843559da03473031c900b074186677faf4ad
Juan Manuel Montenegro <juanmonte2004@gmail.com>
2026-01-22 15:55:08 -0500

    feat: add product entity for inventory module

--- src/main/java/gm/inventarios/InventariosApplication.java
+++ src/main/java/gm/inventarios/InventariosApplication.java
@@ -3,11 +3,22 @@ package gm.inventarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import gm.inventarios.modelo.Producto;
+
 @SpringBootApplication
 public class InventariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventariosApplication.class, args);
+
+		//Prueba de Lombok
+		Producto producto = new Producto();
+		producto.setDescripcion("Camisa Talla mediana");
+		producto.setPrecio(250.75);
+		producto.setExistencia(30);
+
+		//Imprimir el objeto toString de Lombok
+		System.out.println(producto);
 	}
 
 }
